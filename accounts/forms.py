82e31d578d476f6fe0a80be5b92e383d2e50ce9d
david2 <davidfernndez775@yahoo.com>
2024-02-05 19:00:29 -0500

    signup groups assignation solve

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from typing import Any
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
@@ -11,18 +11,21 @@ class MyUserCreateForm(UserCreationForm):
         max_length=30, required=True, help_text='Required. Enter your last name.')
     email = forms.EmailField(
         required=True, help_text='Required. Enter your email.')
+    # Notese que este group no es groups el campo por defecto de User, el valor lo pasamos en la funcion form_valid() dentro de la clase en el views.py
+    group = forms.ModelChoiceField(queryset=Group.objects.all(),
+                                   required=True)
 
     class Meta:
         model = User
         fields = UserCreationForm.Meta.fields + \
-            ('first_name', 'last_name', 'email')
+            ('first_name', 'last_name', 'email', 'group')
 
     def save(self, commit=True):
         user = super().save(commit=False)
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
         user.email = self.cleaned_data['email']
-        user.groups.set(['journalist'])
+        user.group = self.cleaned_data['group']
         if commit:
             user.save()
         return user
--- accounts/views.py
+++ accounts/views.py
@@ -3,12 +3,28 @@ from django.views.generic import CreateView
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.urls import reverse_lazy
 from news_service.forms import JournalistSignupForm
+from django.contrib.auth.models import User, Group
+from news_service.models import Journalist
+from accounts.forms import MyUserCreateForm
 # Create your views here.
 
 
 class SignUp(PermissionRequiredMixin, CreateView):
-    permission_required = "accounts.user.can_add_user"
+    # se definen los permisos requeridos para acceder
+    permission_required = "accounts.add_user"
     form_class = JournalistSignupForm
     # se usa reverse_lazy para garantizar que se guarde el signup antes de ir al login
     success_url = reverse_lazy('news_service:journalists_list')
     template_name = 'accounts/signup.html'
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        # Crear o recuperar el objeto Journalist asociado al usuario, para que exista cuando le asignemos los grupos a los que pertenece
+        journalist, created = Journalist.objects.get_or_create(
+            user=self.object)
+
+        # asignamos el grupo guardado en el campo group del formulario al atributo groups de la clase User, esto es asi porque si no tenemos problemas con la relacion Many to Many
+        self.object.groups.add(self.object.group)
+
+        return response
